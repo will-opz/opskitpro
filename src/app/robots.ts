90a560455052b37a3fb01be318f670fca6b0ac5f
opskitpro <7thqsdhd2r@privaterelay.appleid.com>
2026-04-22 23:10:23 +0900

    feat: add robots.txt route

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://opskitpro.com'
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  }
+}
